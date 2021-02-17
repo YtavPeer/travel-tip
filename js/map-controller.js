d3bc4eae89d27d1812e933bc364a2b3c46f513a7
YtavPeer <ytav.peer@gmail.com>
2021-02-17 19:29:29 +0200

    make copy button

--- js/map-controller.js
+++ js/map-controller.js
@@ -44,6 +44,22 @@ window.onload = () => {
             })
     })
 
+    //this will handle the copy location buttom
+    document.querySelector('.copy-location-btn').addEventListener('click', () => {
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const myParam = urlParams.get('myParam');
+
+        function getParameterByName(name, url = window.location.href) {
+            name = name.replace(/[\[\]]/g, '\\$&');
+            var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, ' '));
+        }
+
+    })
 
 
     initMap()
