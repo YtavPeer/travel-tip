82e9293e6e0f8f5efbc7fbe234586e1605c8ebfa
ShiranCohen86 <shiranc86@gmail.com>
2021-02-17 14:06:12 +0200

    google api

--- js/map-controller.js
+++ js/map-controller.js
@@ -67,7 +67,7 @@ function getPosition() {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAkOmFkBUn6IbejuXmCRMjE0KhuIiXbaY0'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
